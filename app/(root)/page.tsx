95ef1b5818b0b5e077eae70b8c2cd735c0e7845e
Navin Avadhani <knavinavadhani@gmail.com>
2023-08-30 15:19:10 +0530

    Added light and dark mode.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
+import ThemeToggle from "@/components/theme-toggle";
 
 export default function Home() {
   return (
     <main>
       <UserButton afterSignOutUrl="/" />
+      <ThemeToggle />
     </main>
   );
 }
