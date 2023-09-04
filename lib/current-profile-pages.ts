3e20858a64bf744f18550bf2895f048ca8e06de2
Navin Avadhani <knavinavadhani@gmail.com>
2023-09-05 02:33:58 +0530

    Added message API.

---
+++ lib/current-profile-pages.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest } from "next";
+import { getAuth } from "@clerk/nextjs/server";
+import db from "@/lib/db";
+
+export default async function currentProfilePages(req: NextApiRequest) {
+  const { userId } = getAuth(req);
+
+  if (!userId) return null;
+
+  const profile = await db.profile.findUnique({ where: { userId } });
+  return profile;
+}
